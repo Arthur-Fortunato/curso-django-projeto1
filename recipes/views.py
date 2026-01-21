5ad0997ac8905f32e0d3588834c9b3593faca007
Arthur-Fortunato <arthur.r.t.fortunato@gmail.com>
2026-01-21 15:40:07 -0300

    separação da página usando as pastas pages e partials, remoção da pasta temp

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.shortcuts import render
 
 def home(request):
-    return render(request, 'recipes/home.html', context= {
+    return render(request, 'recipes/pages/home.html', context= {
         'name': 'Arthur Ribeiro'
     })
-
-def contato(request):
-    return HttpResponse("CONTATO") 
-
-def sobre(request):
-    return render(request, 'me-apague/temp.html')
